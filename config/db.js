7534a578b033ce3e89ac2fdb372324b2555988d3
Kiefer Weis <kieferweis@protonmail.com>
2022-08-04 15:25:14 -0400

    Add db.js file to set up simple mongoose connection

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      //options to get rid of warnings in the console
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (err) { //if something goes wrong and we can't connect
+    console.error(err) //log the error
+    process.exit(1) //stop the process and exit with failure
+  }
+}
+
+module.exports = connectDB //now we can use this in the app.js file

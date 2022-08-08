956c225b0acabf6e18a1cc13007edb3247ada80e
Kiefer Weis <kieferweis@protonmail.com>
2022-08-08 16:43:55 -0400

    Add CDN stylesheet tags to main.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-// @desc  Login/Landing page
-// @route GET /
+// @desc    Login/Landing page
+// @route   GET /
 router.get('/', (req, res) => {
   res.render('login')
 })
 
-// @desc  Dashboard
-// @route GET /dashboard
+// @desc    Dashboard
+// @route   GET /dashboard
 router.get('/dashboard', (req, res) => {
   res.render('dashboard')
 })

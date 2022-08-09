4efb1a4791725087b751fc4d9ca41f1db325cf2b
Kiefer Weis <kieferweis@protonmail.com>
2022-08-09 16:39:24 -0400

    Set up method override for PUT requests

--- routes/stories.js
+++ routes/stories.js
@@ -61,6 +61,27 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
   }
 })
 
+// @desc    Update story
+// @route   PUT /stories/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+  let story = await Story.findById(req.params.id).lean()
+
+  if (!story) {
+    return res.render('error/404')
+  }
+
+  if (story.user != req.user.id) {
+    res.redirect('/stories')
+  } else {
+    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true, //make sure mongoose fields are valid
+    })
+
+    res.redirect('/dashboard')
+  }
+})
+
   
 
 
